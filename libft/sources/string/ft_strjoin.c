c64a2a33ea01227808c22afbcf8080b99a8c531b
npineau <nofigh@hotmail.fr>
2014-05-05 18:24:01 +0200

    corrected strjoin

--- libft/sources/string/ft_strjoin.c
+++ libft/sources/string/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: npineau <npineau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/28 15:03:39 by npineau           #+#    #+#             */
-/*   Updated: 2014/05/02 15:45:52 by npineau          ###   ########.fr       */
+/*   Updated: 2014/05/05 18:23:40 by npineau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char	*ft_strjoin(const char *s1, const char *s2)
 	int		len2;
 
 	len = ft_strlen(s1);
-	len = ft_strlen(s2);
+	len2 = ft_strlen(s2);
 	new = ft_strnew(len + len2);
 	if (!new)
 		return (NULL);
