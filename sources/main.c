a82928d4ff346abdebb2fba9fa078c6895d5c76e
Matthias De Jesus Oliveira <mde-jesu@student.42.fr>
2014-05-10 13:23:21 +0200

    list sert a rien

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: npineau <npineau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/05/06 14:44:21 by npineau           #+#    #+#             */
-/*   Updated: 2014/05/09 18:32:42 by npineau          ###   ########.fr       */
+/*   Updated: 2014/05/10 13:21:16 by mde-jesu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,11 +36,10 @@ int	main(void)
 {
 	long		i;
 	pthread_t	philos[MAX_PHILO + 1];
-	t_philo		*list;
 	t_env		env;
 
 	i = 0;
-	list = (t_philo *)philosopher((void *)-1);
+	philosopher((void *)-1);
 	env.mlx = mlx_init();
 	env.win = mlx_new_window(env.mlx, X_WIN, Y_WIN, "philo");
 	while (i < MAX_PHILO)
